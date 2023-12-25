77ffc002fd98afda466964061760696a90b55824
= <=>
2023-12-25 14:26:55 +0100

    sjkldfjsld

--- scripts/celsius.c
+++ scripts/celsius.c
@@ -5,15 +5,15 @@ int main(void) {
   int lower, upper, step;
 
   lower = 0;
-  upper = 0;
-  step = 0;
+  upper = 300;
+  step = 20;
 
   fahr = lower;
   while (fahr <= upper)
   {
     celsius = 5 * (fahr - 32) / 9;
     printf("%d\t%d\n", fahr, celsius);
-    fahr += step;
+    fahr = fahr + step;
   }
   return 0;
 }
